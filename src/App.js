cf777fc2c2603cb8717a4ee5dda07eba2b1aabff
easyDev <efscomplex@gmail.com>
2020-09-17 11:19:10 +0200

    404 page not found

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { PageNotFound } from './components/common/404PargeNotFound';
+import { setup, styled, glob } from 'goober'
+import './styles/indes.sass'
+
+setup(React.createElement)
 
 function App() {
   return (
     <div className="App">
-       <h1>404 - Hello World!!</h1>
+       <PageNotFound/>
+       <Credits> 
+          <p>
+             <span className='author'>Author </span> 
+             <span> @easyDev</span>
+         </p>
+          <p> 
+            <span className='github'> Github </span> 
+            <a href='https://github.com/efscomplex'>
+               https://github.com/efscomplex
+            </a>
+         </p>
+      </Credits>
     </div>
   );
 }
-
+glob`.App { position: relative; }`
+const Credits = styled('div')`
+   padding: 2rem;
+   position: absolute;
+   bottom: 1rem;
+   left: 0;
+   color: var(--text-mute);
+   .author, .github {
+      font-weight: bold;
+      color: var(--primary);   
+   }
+`
 export default App;
---
+++ src/components/common/404PargeNotFound.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { styled } from 'goober'
+
+export function PageNotFound(){
+   return (
+      <Page>
+         <Text>
+            <span className='number'>404</span> 
+            <Wrap>
+               <div className='pageNot'>
+                  <span className='page'>PAGE</span> 
+                  <span className='not'>NOT</span>
+               </div>
+            </Wrap>
+            <span className='found'>found</span>
+         </Text>
+      </Page>
+   )
+}
+
+const Wrap = styled('div')`
+   display: inline-flex;
+   align-items: center;
+   .pageNot {
+      display: inline-flex;
+      flex-direction: column;
+      font-size:2.5em;
+      opacity: .7;
+   }
+   .page {
+      font-size: 1em;
+      line-height: .7em;
+   }
+   .not {
+      font-size: 1.2em;
+      line-height: .7em;
+   }
+`
+const Text = styled('span')`
+   text-transform: capitalize;
+   display: flex;
+   align-items: center;
+   transform: translateY(-60%);
+   .number {
+      font-size: 5.5em;
+   }
+   .found {
+      writing-mode: vertical-rl;
+      text-orientation: upright;
+      font-size: .9em;
+      letter-spacing: -.2em;
+      font-weight: 500;
+   }
+`
+const Page = styled('div')`
+   font-size: 2.35rem;
+
+   height: 100vh;
+   with: 100vw;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   background-color: #333;
+   color: white;   
+   * { line-height: 1em;}
+`
